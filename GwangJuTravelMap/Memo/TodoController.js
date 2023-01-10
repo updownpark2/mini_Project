f5cb8a99b035b823af1de5335e5794fd973891bb
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-01-10 14:26:15 +0900

    feat: Todolist를 Localstorage에 저장

--- GwangJuTravelMap/Memo/TodoController.js
+++ GwangJuTravelMap/Memo/TodoController.js
@@ -1,10 +1,21 @@
 import TodoInputView from "./TodoInputView.js";
 
 export default class TodoController {
+  #todo;
   getTodo() {
     TodoInputView((inputValue) => {
-      const toDo = inputValue;
-      console.log(toDo);
+      this.#todo = inputValue;
+      this.saveLocal(this.#todo);
     });
+  }
+
+  saveLocal(todo) {
+    const savedTodo = JSON.parse(localStorage.getItem("todolist"));
+
+    if (savedTodo === null) {
+      localStorage.setItem("todolist", JSON.stringify([todo]));
+      return;
+    }
+    localStorage.setItem(`todolist`, JSON.stringify([todo, ...savedTodo]));
   }
 }
